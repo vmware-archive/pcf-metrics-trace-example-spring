e36dd19447c4d24709a4d1f00045cc272ba8ed22
Adib Saikali <asaikali@gmail.com>
2017-12-13 13:06:26 -0500

    Update script to work with latest CF cli and PWS

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,36 +15,33 @@ SHOPPING_CART_APP_NAME="shopping-cart-$SUFFIX"
 ./gradlew build
 
 cf push $PAYMENTS_APP_NAME \
-    -b https://github.com/cloudfoundry/java-buildpack \
     -p applications/payments/build/libs/payments-trace-example-0.0.1-SNAPSHOT.jar \
-    -m 512M \
+    -m 1GB \
     -k 1024M
 
 
-PAYMENTS_HOST=$(cf app $PAYMENTS_APP_NAME | grep urls | awk '{print $2}')
+PAYMENTS_HOST=$(cf app $PAYMENTS_APP_NAME | grep routes | awk '{print $2}')
 
 cf push $ORDERS_APP_NAME \
-    -b https://github.com/cloudfoundry/java-buildpack \
     -p applications/orders/build/libs/orders-trace-example-0.0.1-SNAPSHOT.jar \
-    -m 512M \
+    -m 1GB \
     -k 1024M \
     --no-start
 
 cf set-env $ORDERS_APP_NAME PAYMENTS_HOST $PAYMENTS_HOST
 cf start $ORDERS_APP_NAME
 
-ORDERS_HOST=$(cf app $ORDERS_APP_NAME | grep urls | awk '{print $2}')
+ORDERS_HOST=$(cf app $ORDERS_APP_NAME | grep routes | awk '{print $2}')
 
 cf push $SHOPPING_CART_APP_NAME \
-    -b https://github.com/cloudfoundry/java-buildpack \
     -p applications/shopping-cart/build/libs/shopping-cart-trace-example-0.0.1-SNAPSHOT.jar \
-    -m 512M \
+    -m 1GB \
     -k 1024M \
     --no-start
 
 cf set-env $SHOPPING_CART_APP_NAME ORDERS_HOST $ORDERS_HOST
 cf start $SHOPPING_CART_APP_NAME
 
-SHOPPING_CART_HOST=$(cf app $SHOPPING_CART_APP_NAME | grep urls | awk '{print $2}')
+SHOPPING_CART_HOST=$(cf app $SHOPPING_CART_APP_NAME | grep routes | awk '{print $2}')
 
 echo Run \`curl $SHOPPING_CART_HOST/checkout\` to verify that the deployment was successful.
