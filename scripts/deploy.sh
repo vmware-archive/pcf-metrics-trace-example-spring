124a4c5c809c0ba2847d0810529a490d2a7ac9c9
jbooherl <jbooher-lasell@pivotal.io>
2019-05-22 13:22:48 -0600

    simplify deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ cd $WORKING_DIR/..
 echo $(pwd)
 
 if [ -z "$SUFFIX" ]; then echo "usage: SUFFIX=<APP_NAME_SUFFIX> ./deploy.sh" && exit; fi
-
+APP_DOMAIN=$${1:-cfapps.io}
 PAYMENTS_APP_NAME="payments-$SUFFIX"
 ORDERS_APP_NAME="orders-$SUFFIX"
 SHOPPING_CART_APP_NAME="shopping-cart-$SUFFIX"
@@ -20,7 +20,7 @@ cf push $PAYMENTS_APP_NAME \
     -k 1024M
 
 
-PAYMENTS_HOST=$(cf app $PAYMENTS_APP_NAME | grep url | awk '{print $2}')
+PAYMENTS_HOST=$PAYMENTS_APP_NAME.$APP_DOMAIN
 
 cf push $ORDERS_APP_NAME \
     -p applications/orders/build/libs/orders-trace-example-0.0.1-SNAPSHOT.jar \
@@ -31,7 +31,7 @@ cf push $ORDERS_APP_NAME \
 cf set-env $ORDERS_APP_NAME PAYMENTS_HOST $PAYMENTS_HOST
 cf start $ORDERS_APP_NAME
 
-ORDERS_HOST=$(cf app $ORDERS_APP_NAME | grep url | awk '{print $2}')
+ORDERS_HOST=$ORDERS_APP_NAME.$APP_DOMAIN
 
 cf push $SHOPPING_CART_APP_NAME \
     -p applications/shopping-cart/build/libs/shopping-cart-trace-example-0.0.1-SNAPSHOT.jar \
@@ -42,7 +42,7 @@ cf push $SHOPPING_CART_APP_NAME \
 cf set-env $SHOPPING_CART_APP_NAME ORDERS_HOST $ORDERS_HOST
 cf start $SHOPPING_CART_APP_NAME
 
-SHOPPING_CART_HOST=$(cf app $SHOPPING_CART_APP_NAME | grep url | awk '{print $2}')
+SHOPPING_CART_HOST=$SHOPPING_CART_APP_NAME.$APP_DOMAIN
 
 echo ""
 echo Run \`curl $SHOPPING_CART_HOST/checkout\` to verify that the deployment was successful.
